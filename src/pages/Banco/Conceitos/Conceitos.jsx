8e5b50908fda2b66afaba8b05ba08cdce645d41f
Luis Henrique <euluishenrique7@gmail.com>
2023-07-28 23:10:30 -0300

    adições nos conceitos

--- src/pages/Banco/Conceitos/Conceitos.jsx
+++ src/pages/Banco/Conceitos/Conceitos.jsx
@@ -16,33 +16,40 @@ function Conceitos() {
       <section className='Espiritos'>
         <h2 className='Conceitosespiritos'>Espíritos:</h2>
 
-        Seres feitos puramente de mana vindos do centro do mundo de Ravager. Os espíritos são divididos em 4 categorias: Espíritos Menores, Espíritos Maiores, Espíritos Artificiais e Grandes Espíritos.
+        Seres feitos puramente de mana vindos do centro do mundo. Os espíritos são divididos em 4 categorias: Espíritos Menores, Espíritos Maiores, Espíritos Artificiais e Grandes Espíritos.
 
-        <h3 className='espacoconceitos'>Espíritos menores:</h3>
+        <h3 className='Conceitoespiritossubtitulos'>Espíritos menores:</h3>
         Espíritos menores normalmente não possuem uma inteligência apurada, e sua consciência e corpo físico são extremamente limitados, se manifestando apenas como uma pequena esfera de energia mágica.
 
-        <h3 className='espacoconceitos'>Espíritos maiores:</h3>
+        <h3 className='Conceitoespiritossubtitulos'>Espíritos maiores:</h3>
         Espíritos que evoluíram ao ponto de possuírem uma forma física melhor desenvolvida. Além de sua forma corpórea, Espíritos maiores possuem uma consciência equivalente a de uma “criatura inteligente” (como elfos, humanos etc), além de, obviamente, serem mais poderosos que os Espíritos menores.
 
-        <h3 className='espacoconceitos'>Grandes Espíritos:</h3>
+        <h3 className='Conceitoespiritossubtitulos'>Grandes Espíritos:</h3>
         Espíritos que ascenderam e se tornaram absurdamente poderosos, são classificados como os mais fortes dentre todos os espíritos.
 
-        <h3 className='espacoconceitos'>Espíritos artificiais:</h3>
+        <h3 className='Conceitoespiritossubtitulos'>Espíritos artificiais:</h3>
         Espíritos criados artificialmente por alguém. São poucos no mundo que sabem como criar um Espírito artificial. 
         Espíritos artificiais podem ser tanto Maiores como Menores (podem também ascender para Grande Espírito), tudo depende de quem o cria.
       </section>
 
       <section className='Contratos'>
-        <h2 className='Conceitocontratos'>Acordos:</h2>
+        <h2 className='Conceitocontratos'>Contratos:</h2>
         Um acordo mágico entre dois ou mais indivíduos.
         <p/>A diferença entre um Contrato e um acordo formal é a consequência gerada com o descumprimento do mesmo. Ao quebrar um Contrato, o indivíduo que faltou com a palavra receberá uma consequência pré estabelecida pelos dois participantes.
         <p/>As condições de um Contrato são especificadas antes do acordo ser firmado, não podendo haver alterações a não ser que ambas as partes concordem em encerrá-lo e refazê-lo com novos termos.
         <p/>Para um Contrato ser quebrado, é necessário que ambas as partes concordem que o mesmo seja encerrado.
         <p/>Contratos se limitam apenas aos indivíduos nele inseridos, um terceiro não poderá ser afetado pelas condições do acordo firmado.
 
-        <h3 className='espacoconceitos'>Exemplo de Contrato 1:</h3> Um troll realiza um Contrato com um gnomo, onde as condições são do troll proteger o gnomo da melhor forma possível, e caso o Contrato seja descumprido o troll queimará.
+        <h3 className='Conceitocontratossubtitulos'>Exemplo de Contrato 1:</h3> Um troll realiza um Contrato com um gnomo, onde as condições são do troll proteger o gnomo da melhor forma possível, e caso o Contrato seja descumprido o troll queimará.
 
-        <h3 className='espacoconceitos'>Exemplo de Contrato 2:</h3> Um espírito de fogo realiza um Juramento com um humano, onde nele o humano terá acesso a mana e algumas magias concedidas pelo espírito, em troca, o humano deverá tocar flauta para o espírito toda noite antes de dormir. Ambos firmaram que, em caso de descumprimento, nada aconteceria a nenhuma das partes.
+        <h3 className='Conceitocontratossubtitulos'>Exemplo de Contrato 2:</h3> Um espírito de fogo realiza um Contrato com um humano, onde nele o humano terá acesso a mana e algumas magias concedidas pelo espírito, em troca, o humano deverá tocar flauta para o espírito toda noite antes de dormir. Ambos firmaram que, em caso de descumprimento, nada aconteceria a nenhuma das partes.
+      
+        <h3 className='Conceitocontratossubtitulos'>Contrato de Sangue:</h3>
+        <p/>Contratos realizados entre indivíduos que se estendem por gerações. Contrato de Sangue podem ser quebrados desde que o indivíduo seja do mesmo sangue de quem firmou o Pacto e que o mesmo já esteja morto.
+
+        <p/>Exemplo de Contrato de Sangue: Um espírito maior realiza um Contrato de Sangue com um rei, onde nele o espírito maior protegerá as terras do rei enquanto sua família estiver no trono, e em troca, a família do rei deve conceder um banquete todo mês ao espírito. 
+        Ambos firmaram que, em caso de descumprimento por parte do espírito, o mesmo morrerá, e, em caso de descumprimento por parte da família do rei, o espírito irá assassinar todos os membros da linhagem.
+
       </section>
 
      </div>
